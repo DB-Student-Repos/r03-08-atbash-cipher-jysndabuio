1a42b85594122726484be4cf25333e2e64cb4ab8
jysndabuio <jysndabu@outlook.com>
2024-04-14 15:40:59 +0200

    Solution but still  need to simplify

--- src/lib.rs
+++ src/lib.rs
@@ -12,17 +12,38 @@ pub fn collection()-> HashMap<char, char> {
 }
 
 pub fn encode(plain: &str) -> String {
-    let word: String = plain.chars().filter(|&x| x.is_ascii_alphabetic()).collect()
     let code_chars = collection();
-    let mut cipher = String::new(); 
-    for x in word.chars(){
-        match x {
-            x.is_digit(10) => cipher.push(x),
-            _ => cipher.push(*code_chars.get(&x).unwrap_or(&x)),
-        }
-    }
+    let word: String = plain.chars()
+                            .filter(|&x| x.is_ascii_alphabetic() || x.is_ascii_digit())
+                            .collect::<String>()
+                            .to_lowercase();
+
+    let converted_word: String = word.chars()
+                            .map(|x| {match x {
+                                'a'..='z' => *code_chars.get(&x).unwrap_or(&x),
+                                _ => x,
+                            }})
+                            .collect();
+
+    let sliced_str: String = converted_word.chars()
+        .enumerate()
+        .map(|(i, c)| if i % 5 == 0 && i != 0 { format!(" {}",c) } else { c.to_string() })
+        .collect();
+
+    sliced_str
 }
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
+    let code_chars = collection();
+    let word: String = cipher.replace(" ", "");
+    let mut converted_word = String::new();
+    for  x in word.chars() {
+        let decoded_char = match code_chars.get(&x) {
+            Some(plain) => *plain,
+            None => x,
+        };
+        converted_word.push(decoded_char);
+    }
+    converted_word
 
 }
